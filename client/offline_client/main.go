4b54cdb1440c0f6ce7f6bcf335bada16cdfb8898
gyj233 <1481628125@qq.com>
2022-03-23 20:31:51 +0800

    feat:离线服务支持query的删除

--- client/offline_client/main.go
+++ client/offline_client/main.go
@@ -29,24 +29,50 @@ func main() {
 	offlineClient := searchPb.NewFeatureService("Search_System", service.Client())
 
 	rsp, err := offlineClient.Set(context.Background(), &searchPb.OfflineRequest{
+		// // 添加一个query
 		// Type: searchPb.SetType_TYPE_ADD,
 		// Querys: []*searchPb.Query{
 		// 	{
 		// 		QueryId:   "0926",
 		// 		QueryName: "xx",
 		// 		Kind:      "食物",
-		// 		Feature:   `{"味道":"甜", "价格":"奢侈", "颜色":"白", "口感":"软"}`,
+		// 		Feature:   `{"味道":"甜", "价格":"奢侈", "颜色":"白", "口感":"软","品牌":"小象","品类":"糖"}`,
 		// 	},
 		// },
-		Type: searchPb.SetType_TYPE_UPDATE,
-		Querys: []*searchPb.Query{
-			{
-				QueryId:   "0926",
-				QueryName: "xx",
-				Kind:      "食物",
-				Feature:   `{"味道":"苦", "价格":"奢侈", "颜色":"白", "口感":"硬"}`,
-			},
-		},
+		// // 再添加一个query  有相同特征
+		// Type: searchPb.SetType_TYPE_UPDATE,
+		// Querys: []*searchPb.Query{
+		// 	{
+		// 		QueryId:   "1030",
+		// 		QueryName: "gyj",
+		// 		Kind:      "食物",
+		// 		Feature:   `{"味道":"甜", "价格":"奢侈", "颜色":"黄", "口感":"硬","品牌":"小象","品类":"糖"}`,
+		// 	},
+		// },
+		// // 修改相同特征为不同特征
+		// Type: searchPb.SetType_TYPE_UPDATE,
+		// Querys: []*searchPb.Query{
+		// 	{
+		// 		QueryId:   "1030",
+		// 		QueryName: "gyj",
+		// 		Kind:      "食物",
+		// 		Feature:   `{"味道":"甜", "价格":"奢侈", "颜色":"黄", "口感":"硬","品牌":"大象","品类":"糖"}`,
+		// 	},
+		// },
+		// // 删除第二个query  先删第二个query是为了验证 索引表中靠后的queryID能够正常删除
+		// Type: searchPb.SetType_TYPE_DELETE,
+		// Querys: []*searchPb.Query{
+		// 	{
+		// 		QueryId: "1030",
+		// 	},
+		// },
+		// // 删除第一个query
+		// Type: searchPb.SetType_TYPE_DELETE,
+		// Querys: []*searchPb.Query{
+		// 	{
+		// 		QueryId: "0926",
+		// 	},
+		// },
 	})
 
 	fmt.Printf("rsp:(%v) err(%v)", rsp, err)
